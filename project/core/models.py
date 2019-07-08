02ace3856a6d67537af83e99acc21e4dc867ded5
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-07-07 22:19:10 -0300

    fix digitation error

--- project/core/models.py
+++ project/core/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class TimeStampedModel(models.Model):
-    created = models.DataTimeField(
+    created = models.DateTimeField(
             'criado em',
             auto_now_add=True,
             auto_now=False
     )
-    modified = models.DataTimeField(
+    modified = models.DateTimeField(
             'modificado em',
             auto_now_add=False,
             auto_now=True
