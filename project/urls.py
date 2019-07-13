969de200814546e969254e733df1056c57abac4a
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-07-13 18:11:47 -0300

    create product list, product form add new product

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include,  path
 
 urlpatterns = [
     path('', include('project.core.urls')),
+    path('product/', include('project.product.urls')),
     path('admin/', admin.site.urls),
 ]
