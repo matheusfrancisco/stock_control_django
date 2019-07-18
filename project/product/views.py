b07123f44ced6f25fa91cc82c02801ce9c82ac48
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-07-17 22:05:45 -0300

    update product

--- project/product/views.py
+++ project/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from .models import Produto
 from .forms import ProdutoForm
 # Create your views here.
@@ -26,3 +26,9 @@ class ProdutoCreate(CreateView):
     model=Produto
     template_name='produto_form.html'
     form_class = ProdutoForm
+
+class ProdutoUpdate(UpdateView):
+    model=Produto
+    template_name='produto_form.html'
+    form_class = ProdutoForm
+
